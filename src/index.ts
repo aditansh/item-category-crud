d91b2d07fc4fe603acb799f2d08ecd63d6639a25
Aaditya Mahanta <aadityamahanta07@gmail.com>
2024-05-18 13:32:02 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+// import { loadConstants } from "./constants";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+
+// loadConstants();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send({ status: "success", message: "pong!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
